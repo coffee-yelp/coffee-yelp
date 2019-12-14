6455b9d0843384d9b6f1bfc81830bf25d2448d70
Matt Garcia <mattrg1105@gmail.com>
2019-12-14 06:42:30 -0800

    tab navigator working with icons

--- App.js
+++ App.js
@@ -4,22 +4,55 @@ import store from './src/redux/store';
 import Map from './src/components/Map';
 import { Header } from 'react-native-elements';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
+
 
 import Home from './src/components/Home';
 import MapPage from './src/components/MapPage';
 
-import { createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name='home' size={25} color={tintColor} />
+      )
+    }
+    },
+    Map: {
+      screen: MapPage,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor}) => (
+        <Icon name='map' size={25} color={tintColor} />
+      )
+    }
+    }
+
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#1e90ff'
+    }
+  }
+)
 
 const RootStack = createStackNavigator({
-  Home: Home,
-  MapPage: MapPage,
+  Home: bottomTabNavigator
 })
+
+const AppContainer = createAppContainer(RootStack);
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <RootStack />
+        <AppContainer />
       </Provider>
     );
   }
